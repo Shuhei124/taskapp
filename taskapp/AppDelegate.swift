557abf9bfcbd0059aa945f312d186b396d71c699
Shuhei <shuhei.git@outlook.com>
2021-01-03 17:02:55 +0900

    テキスト内容

--- taskapp/AppDelegate.swift
+++ taskapp/AppDelegate.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import UserNotifications    // 追加
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDelegate  {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // ユーザに通知の許可を求める --- ここから ---
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            // Enable or disable features based on authorization
+        } // --- ここまで追加 ---
+        center.delegate = self
         return true
     }
 
@@ -30,7 +36,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    // アプリがフォアグラウンドの時に通知を受け取ると呼ばれるメソッド --- ここから ---
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    } // --- ここまで追加 ---
 
 }
 
